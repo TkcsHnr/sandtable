f95353cb45a34537a33c6fe817365de35834ceef
TkcsHnr <tkcshnr@gmail.com>
2025-02-26 12:07:44 +0100

    Page server load check auth

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,17 @@
 import type { PageServerLoad } from './$types';
-export const load = (async () => {
-	const patterns = import.meta.glob('/static/patterns/*.gcode' );
+import { WEBSOCKET_PASSWORD } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
+export const load = (async ({ cookies }) => {
+	const websocket_password = cookies.get('websocket_password') || '';
+	if (websocket_password != WEBSOCKET_PASSWORD) {
+		throw redirect(302, '/auth');
+	}
+
+	const patterns = import.meta.glob('/static/patterns/*.gcode');
 
 	const patternNames = Object.keys(patterns).map((filePath) =>
 		filePath.replace('/static/patterns/', '')
 	);
 
-	return { patterns: patternNames };
+	return { patterns: patternNames, websocket_password };
 }) satisfies PageServerLoad;
