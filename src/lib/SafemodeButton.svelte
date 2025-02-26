35a489a6ab03d6489ad9599875ffb83e11560ca9
TkcsHnr <tkcshnr@gmail.com>
2025-02-26 12:02:26 +0100

    Safemode control button

---
+++ src/lib/SafemodeButton.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { machineStats } from "./stores";
+	import { sendSafemode } from "./websocket";
+
+    function toggleSafemode() {
+        $machineStats.safemode = !$machineStats.safemode;
+        sendSafemode($machineStats.safemode)
+    }
+
+</script>
+
+<button class="btn btn-square" aria-label="safemode" onclick={toggleSafemode}>
+    <i class="fa-solid {$machineStats.safemode ? 'fa-lock' : 'fa-lock-open'}"></i>
+</button>
