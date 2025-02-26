83a2bd1a108c5424a5c973188dbbb1f88cae647d
TkcsHnr <tkcshnr@gmail.com>
2025-02-26 18:02:32 +0100

    espConnected boolean store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -23,5 +23,5 @@ const defaultMachineStats: MachineStats = {
 
 export const machinePatterns = writable<string[]>([]);
 export const socketState = writable<number>(3);
-export const espConnected = writable<number>(1);
+export const espConnected = writable<boolean>(false);
 export const machineStats = writable<MachineStats>(defaultMachineStats);
