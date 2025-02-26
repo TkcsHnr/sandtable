b2b434bc21f4642a71ccf88bae1e1fc579f3579c
TkcsHnr <tkcshnr@gmail.com>
2025-02-26 18:01:14 +0100

    Esp pattern file list

---
+++ src/lib/Patterns.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { machinePatterns, machineStats } from './stores';
+	import { sendDeletePattern, sendStart } from './websocket.js';
+
+	$machinePatterns = [
+		'/giganiga.bin',
+		'/sevenhills.bin',
+		'/abrakadabra.bin',
+		'/avengers.bin',
+		'/chilliardobando.bin',
+		'/fuckthepolice.bin'
+	];
+</script>
+
+<div class="flex flex-col p-4 gap-4 rounded-box bg-base-200 h-fit">
+    <h2 class="text-lg font-bold px-2">Patterns on ESP:</h2>
+	{#each $machinePatterns as pattern}
+		<div class="flex gap-2 items-baseline">
+			<button
+				class="btn btn-sm btn-square btn-ghost"
+				aria-label="start"
+				onclick={() => sendStart(pattern)}
+                disabled={!$machineStats.homed}
+			>
+				<i class="fa-solid fa-play"></i>
+			</button>
+			<p>{pattern.replace('/', '').replace('.bin', '')}</p>
+			<button
+				class="btn btn-sm btn-square btn-ghost ml-auto"
+				aria-label="delete"
+				onclick={() => sendDeletePattern(pattern)}
+			>
+				<i class="fa-solid fa-trash text-error"></i>
+			</button>
+		</div>
+	{/each}
+</div>
